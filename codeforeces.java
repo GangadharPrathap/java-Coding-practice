18b66648a3868b72519ba666ef1e66d60580c504
24f3002979-dev <24f3002979@ds.study.iitm.ac.in>
2025-08-15 22:43:45 +0530

    commiting the codeforeces files

---
+++ codeforeces.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        long y = sc.nextLong();
+        System.out.println(25);
+    }
+}
--- tuf/secondlargest.java
+++ tuf/secondlargest.java
@@ -9,16 +9,19 @@ public class secondlargest {
 
         nums = new int[n];
         System.out.println("Enter the values of the array");
-        for(int i =0; i<n;i++){
+        for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
         int largest = nums[0];
         int slargest = -1;
-        for (int i =0;i<n;i++){
-            if(nums[i]>largest){
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > largest) {
                 slargest = largest;
                 largest = nums[i];
+            } else if (nums[i] < largest && nums[i] > slargest) {
+                slargest = nums[i];
             }
-        }System.out.println(slargest);
+        }
+        System.out.println(slargest);
     }
 }
